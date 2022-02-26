f59d6e7f488bfd4f075dbacb345dc1e650c62920
Neil Ranada <neil.ranada@gmail.com>
2022-02-25 23:47:22 -0800

    Added ability to print filename of one program argument

--- ls_demo.c
+++ ls_demo.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <string.h>
 
-int main()
+void check_arguments(char** argc, struct dirent *entry);
+
+int main(int argv, char** argc)
 {
+   
     DIR *folder;
     struct dirent *entry;
     int files = 0;
@@ -14,16 +18,35 @@ int main()
         return(1);
     }
 
-    while( (entry=readdir(folder)) )
+    while( (entry = readdir(folder)) )
     {
         files++;
-        printf("File %3d: %s\n",
+
+        if (argv == 1)
+        {
+            printf("File %3d: %s\n",
                 files,
                 entry->d_name
               );
+        }
+        else
+        {
+            check_arguments(argc, entry);
+        }
     }
 
     closedir(folder);
 
     return(0);
+}
+
+void check_arguments(char** argc, struct dirent *entry)
+{
+    // printf("Argc[1]: %s\n", argc[1]);         
+    // printf("File name: %s\n", entry->d_name);
+
+    if (strcmp(argc[1], entry->d_name) == 0)
+    {
+        printf("%s\n", entry->d_name);
+    }
 }
